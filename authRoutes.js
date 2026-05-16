fd42fbdc94d52ccf169601cbf657f2ebca4e5465
pavantej370-hue <pavantej370@gmail.com>
2026-05-16 19:07:06 +0530

    Create authRoutes.js

---
+++ authRoutes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+router.post('/register', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        await user.save();
+
+        res.status(201).json({ message: 'User Registered' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+        res.json({ token });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
